6f307621d25e728ac00334c6a888cd5b8422407d
GayathriDevi15 <159441252+GayathriDevi15@users.noreply.github.com>
2024-03-06 12:31:56 +0530

    Update FirstGIT.java

--- src/com/cg/Demo/FirstGIT.java
+++ src/com/cg/Demo/FirstGIT.java
@@ -4,7 +4,7 @@ public class FirstGIT {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("Welcome to GIT");
+       System.out.println("Welcome to GIT-changed from Github");
 	}
 
 }
