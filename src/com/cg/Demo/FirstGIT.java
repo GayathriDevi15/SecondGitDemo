3a75ccf6566a454869d638b6357e23e6a46311d9
Devi <satti-gayathri.devi@capgemini.com>
2024-03-06 12:25:10 +0530

    First commit

---
+++ src/com/cg/Demo/FirstGIT.java
@@ -0,0 +1,11 @@
+package com.cg.Demo;
+
+public class FirstGIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Welcome to GIT");
+       System.out.println("Code from STS to GIT- First branch");
+	}
+
+}
---
+++ src/com/cg/Demo/SecondGIT.java
@@ -0,0 +1,7 @@
+package com.cg.Demo;
+
+public class SecondGIT {
+   public static void main(String args[]) {
+	   System.out.println("Second demo from STS");
+   }
+}
